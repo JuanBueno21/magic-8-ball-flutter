47dc5ad18cbcfcc720e68fe94d0c9d9d3b529b93
JuanBueno21 <juancamilobuenomintic@gmail.com>
2024-03-08 16:37:23 -0500

    [MagicBall] new project uploaded
    this is a new project where you can do different ask

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,55 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          body: AskMe(),
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text(
+              "Ask Me Anything",
+              style: TextStyle(color: Colors.white),
+            ),
+            backgroundColor: Colors.blue.shade800,
+          ),
+        ),
       ),
     );
+
+class AskMe extends StatefulWidget {
+  const AskMe({Key? key}) : super(key: key);
+
+  @override
+  State<AskMe> createState() => _AskMeState();
+}
+
+class _AskMeState extends State<AskMe> {
+  int ballAnswer = 1;
+
+  void changeTheState() {
+    setState(() {
+      ballAnswer = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeTheState();
+
+                },
+              child: Image.asset("images/ball$ballAnswer.png"),
+
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
